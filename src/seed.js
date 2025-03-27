67b6aa47c93bc19ef07b0c7b265f0ddd7c1fa352
zhihao <1348261663@qq.com>
2025-03-27 17:12:33 +0800

    优化数组监听

--- src/seed.js
+++ src/seed.js
@@ -36,6 +36,7 @@ function Seed (el, options) {
     this.scope.$destroy = this._destroy.bind(this)
     this.scope.$dump    = this._dump.bind(this)
     this.scope.$index   = options.index
+    this.scope.$parent  = options.parentSeed && options.parentSeed.scope
 
     this._controller = controller
 
